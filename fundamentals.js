aaac3a28487b0696836fb93c8e65ea8d90531992
Eric Butler <72872237+EricDeBuitleir@users.noreply.github.com>
2022-09-15 14:27:41 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,15 @@
 const aCar = {
+
+    previous_owners: [ " Pat Smith - 1 Main Street " , " Shiela Dwyer - 2 High Street "],
+
     owner : "Joe Bloggs",
+    milage : "10000",
+    colour : {
+        exterior : "red",
+        interior : "fabric",
+        texture : "leather",
+        sahde : "cream"
+    },
     registration : {
         year : "201",
         code : "WD",
@@ -18,5 +28,5 @@ const aCar = {
 
 console.log(aCar.owner + ' drives a ' + aCar.type.make)
 console.log(aCar.registration.year + "-" + aCar.registration.code + "-" + aCar.registration.num)
-
-
+console.log("It is a " + aCar.colour.exterior + "car," + " with a " + aCar.milage + " milage,"  + " with " + aCar.colour.texture + " interior ")
+console.log(' First owner : ' + aCar.previous_owners [0] )
